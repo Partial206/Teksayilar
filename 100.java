34d86b16a43e51d3af16cc714a6aede674798fb5
Partial <133568898+Partial206@users.noreply.github.com>
2023-05-30 21:24:50 +0300

    Create 100.java

---
+++ 100.java
@@ -0,0 +1,8 @@
+public class 100 {
+    public static void main(String[] args) 
+        for(int i=1;i<=100;i+=2){ /* 100'e Kadar i Değerine 2 ekler bu sayede tek sayılar ortaya çıkar */ 
+            System.out.println(i); /* 100 e kadar kendini tekrarlar ve tek sayıları yazar */
+        } /* */
+        System.out.println("Bitti."); /* döngü bittiğinde "bitti" yazar*/
+    }
+}
