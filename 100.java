fe78e9b67995452c1fcb5f389eea60652ee395c1
Partial <133568898+Partial206@users.noreply.github.com>
2023-05-30 21:28:11 +0300

    Update 100.java

--- 100.java
+++ 100.java
@@ -1,5 +1,5 @@
 public class 100 {
-    public static void main(String[] args) 
+    public static void main(String[] args) {
         for(int i=1;i<=100;i+=2){ /* 100'e Kadar i Değerine 2 ekler bu sayede tek sayılar ortaya çıkar */ 
             System.out.println(i); /* 100 e kadar kendini tekrarlar ve tek sayıları yazar */
         } /* */
